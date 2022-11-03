a0d236fc80ec4747b4fc742c735b71cbd98cc23d
Vlad Ageysha <ander123fakk@gmail.com>
2022-11-03 15:49:25 +0300

    Made slide for menu what i want

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-window.addEventListener('scroll', (e) => {
+const navbarBtn = document.querySelector('.header__btn-navbar');
+
+navbarBtn.addEventListener('click', () => {
+    const menuOverlay = document.querySelector('.mobile-menu__overlay');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    if (menuOverlay.classList.contains('mobile-menu__overlay_open')) {
+        setTimeout(() => {
+            menuOverlay.classList.remove('mobile-menu__overlay_open');
+            menuOverlay.style.display = 'none';
+        }, 350);
+
+        mobileMenu.classList.remove('mobile-menu_open');
+    }
+    else {
+        menuOverlay.style.display = 'block';
+        setTimeout(() => {
+            menuOverlay.classList.add('mobile-menu__overlay_open')
+            mobileMenu.classList.add('mobile-menu_open');
+        }, 0);
+        
+    }
+});
+
+window.addEventListener('scroll', () => {
     const header = document.querySelector('.header');
     const content = document.querySelector('.content');
 
@@ -11,4 +35,3 @@ window.addEventListener('scroll', (e) => {
         content.classList.remove('content_header-fixed');
     }
 });
-
